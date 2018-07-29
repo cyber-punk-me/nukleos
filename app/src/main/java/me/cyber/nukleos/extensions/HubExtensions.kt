844f7413438b75d0d85b58704f0447c4b249b575
garastard <mb@blackbeautystudio.com>
2018-07-29 18:30:26 +0300

    abstract sensor, Sensor reworking

--- app/src/main/java/me/cyber/nukleos/extensions/HubExtensions.kt
+++ app/src/main/java/me/cyber/nukleos/extensions/HubExtensions.kt
@@ -1,29 +1,32 @@
 package me.cyber.nukleos.extensions
 
 import android.util.Log
-import com.thalmic.myo.GattCallback
 import com.thalmic.myo.Hub
 import com.thalmic.myo.Myo
 import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
 import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
+import me.cyber.nukleos.sensor.ISensor
+import me.cyber.nukleos.sensor.ISensorListener
+import me.cyber.nukleos.sensor.ISensorSettings
 import java.util.*
 
 /**
  * Extensions for hub
  */
 
-fun Hub.setRXListener() = Flowable.create<GattValues>({ e ->
-    this.addGattValueListener(object : GattCallback.ValueListener {
-        override fun onCharacteristicChanged(myo: Myo, uuid: UUID, bArr: ByteArray) {
-            e.onNext(GattValues(myo, uuid, bArr))
+fun ISensor.setRXListener() = Flowable.create<GattValues>({ e ->
+    this.addListener(object : ISensorListener {
+        override fun onCharacteristicChanged(sensorSettings: ISensorSettings, uuid: UUID, bArr: ByteArray) {
+            e.onNext(GattValues(sensorSettings, uuid, bArr))
         }
     })
     e.setCancellable { Log.e("--ONERROR--", "---focus on GattValueListener. It makes something wrong!---") }
 }, BackpressureStrategy.BUFFER)
 
-data class GattValues(val myo: Myo, val uuid: UUID, val byteArray: ByteArray)
+
+data class GattValues(val myo: ISensorSettings, val uuid: UUID, val byteArray: ByteArray)
 
 fun Disposable?.safeDispose() = this?.let {
     if (!it.isDisposed) {
