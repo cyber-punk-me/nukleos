f11e36d4eee1601a161b5be723ce4cd821635298
kyr7 <kirillskiy@gmail.com>
2019-02-05 00:57:43 +0300

    connecting to synaps via usb

--- app/src/main/java/me/cyber/nukleos/ui/MainActivity.kt
+++ app/src/main/java/me/cyber/nukleos/ui/MainActivity.kt
@@ -1,12 +1,17 @@
 package me.cyber.nukleos.ui
 
+import android.content.*
 import android.os.Bundle
+import android.os.Handler
+import android.os.IBinder
+import android.os.Message
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import android.widget.Toast
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
@@ -18,6 +23,8 @@ import me.cyber.nukleos.ui.find.FindSensorFragment
 import me.cyber.nukleos.ui.predict.PredictFragment
 import me.cyber.nukleus.R
 import javax.inject.Inject
+import me.cyber.nukleos.synaps.UsbService
+import java.lang.Thread.sleep
 
 class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
 
@@ -26,8 +33,48 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
 
     private var navigationBlocked = false
 
+    private var usbService: UsbService? = null
+    private var usbHandler: UsbHandler? = null
+    private val usbConnection = object : ServiceConnection {
+        override fun onServiceConnected(arg0: ComponentName, arg1: IBinder) {
+            usbHandler = UsbHandler(this@MainActivity)
+            usbService = (arg1 as UsbService.UsbBinder).service
+            usbService?.setHandler(usbHandler)
+            //
+            Thread {
+                sleep(1000)
+                usbService?.write("v".toByteArray())
+            }.start()
+        }
+
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            usbService = null
+        }
+    }
+
     override fun supportFragmentInjector(): AndroidInjector<Fragment> {
         return fragmentDispatchingAndroidInjector
+    }
+
+
+    /*
+     * Notifications from UsbService will be received here.
+     */
+    private val usbReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            when (intent.getAction()) {
+                UsbService.ACTION_USB_PERMISSION_GRANTED // USB PERMISSION GRANTED
+                -> Toast.makeText(context, "USB Ready", Toast.LENGTH_SHORT).show()
+                UsbService.ACTION_USB_PERMISSION_NOT_GRANTED // USB PERMISSION NOT GRANTED
+                -> Toast.makeText(context, "USB Permission not granted", Toast.LENGTH_SHORT).show()
+                UsbService.ACTION_NO_USB // NO USB CONNECTED
+                -> Toast.makeText(context, "No USB connected", Toast.LENGTH_SHORT).show()
+                UsbService.ACTION_USB_DISCONNECTED // USB DISCONNECTED
+                -> Toast.makeText(context, "USB disconnected", Toast.LENGTH_SHORT).show()
+                UsbService.ACTION_USB_NOT_SUPPORTED // USB NOT SUPPORTED
+                -> Toast.makeText(context, "USB device not supported", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -76,6 +123,45 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
         }
     }
 
+    public override fun onResume() {
+        super.onResume()
+        setFilters()  // Start listening notifications from UsbService
+        startService(UsbService::class.java, usbConnection, null) // Start UsbService(if it was not started before) and Bind it
+    }
+
+
+    private fun startService(service: Class<*>, serviceConnection: ServiceConnection, extras: Bundle?) {
+        if (!UsbService.SERVICE_CONNECTED) {
+            val startService = Intent(this, service)
+            if (extras != null && !extras.isEmpty) {
+                val keys = extras.keySet()
+                for (key in keys) {
+                    val extra = extras.getString(key)
+                    startService.putExtra(key, extra)
+                }
+            }
+            startService(startService)
+        }
+        val bindingIntent = Intent(this, service)
+        bindService(bindingIntent, serviceConnection, Context.BIND_AUTO_CREATE)
+    }
+
+    private fun setFilters() {
+        val filter = IntentFilter()
+        filter.addAction(UsbService.ACTION_USB_PERMISSION_GRANTED)
+        filter.addAction(UsbService.ACTION_NO_USB)
+        filter.addAction(UsbService.ACTION_USB_DISCONNECTED)
+        filter.addAction(UsbService.ACTION_USB_NOT_SUPPORTED)
+        filter.addAction(UsbService.ACTION_USB_PERMISSION_NOT_GRANTED)
+        registerReceiver(usbReceiver, filter)
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        unregisterReceiver(usbReceiver)
+        unbindService(usbConnection)
+    }
+
     fun navigateToPage(pageId: Int) {
         view_pager.currentItem = pageId
     }
@@ -92,6 +178,27 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
 
         override fun getItem(position: Int): Fragment {
             return fragmentList[position]
+        }
+    }
+
+    /*
+     * This handler will be passed to UsbService. Data received from serial port is displayed through this handler
+     */
+    private class UsbHandler(val activity: MainActivity) : Handler() {
+
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                UsbService.MESSAGE_FROM_SERIAL_PORT -> {
+                    val data = msg.obj as String
+                    Toast.makeText(activity, data, Toast.LENGTH_LONG).show()
+                }
+                UsbService.CTS_CHANGE -> Toast.makeText(activity, "CTS_CHANGE", Toast.LENGTH_LONG).show()
+                UsbService.DSR_CHANGE -> Toast.makeText(activity, "DSR_CHANGE", Toast.LENGTH_LONG).show()
+                UsbService.SYNC_READ -> {
+                    val data = msg.obj as String
+                    Toast.makeText(activity, data, Toast.LENGTH_LONG).show()
+                }
+            }
         }
     }
 }
