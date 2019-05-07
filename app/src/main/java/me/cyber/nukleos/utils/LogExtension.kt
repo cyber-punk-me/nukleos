dde6138a2164b7a11af109b28ded6bc723822925
Arseny Chernyaev <arseny.chernyaev@jetbrains.com>
2019-05-07 16:59:42 +0300

    cancel previous toast

--- app/src/main/java/me/cyber/nukleos/utils/LogExtension.kt
+++ app/src/main/java/me/cyber/nukleos/utils/LogExtension.kt
@@ -3,6 +3,18 @@ package me.cyber.nukleos.utils
 import android.widget.Toast
 import me.cyber.nukleos.App
 
+private var currentToast: Toast? = null
 
-fun String.showShortToast() = Toast.makeText(App.applicationComponent.getAppContext(), this, Toast.LENGTH_SHORT).show()
-fun String.showLongToast() = Toast.makeText(App.applicationComponent.getAppContext(), this, Toast.LENGTH_LONG).show()
+fun String.showShortToast() {
+    currentToast?.cancel()
+    val toast = Toast.makeText(App.applicationComponent.getAppContext(), this, Toast.LENGTH_SHORT)
+    toast.show()
+    currentToast = toast
+}
+
+fun String.showLongToast() {
+    currentToast?.cancel()
+    val toast = Toast.makeText(App.applicationComponent.getAppContext(), this, Toast.LENGTH_LONG)
+    toast.show()
+    currentToast = toast
+}
