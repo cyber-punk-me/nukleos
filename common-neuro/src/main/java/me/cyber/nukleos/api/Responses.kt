0a1691d7ee2c88e9de8480ac4df49694bdaaced9
kyr7 <kirillskiy@gmail.com>
2020-01-07 18:30:27 +0300

    api date -> long

--- common-neuro/src/main/java/me/cyber/nukleos/api/Responses.kt
+++ common-neuro/src/main/java/me/cyber/nukleos/api/Responses.kt
@@ -6,13 +6,13 @@ enum class RunState {
     NEW, RUNNING, COMPLETE, ERROR
 }
 
-data class ScriptMeta(val scriptId: UUID, val created: Date)
+data class ScriptMeta(val scriptId: UUID, val created: Long)
 
-data class DataMeta(val dataId: UUID, val created: Date, val updated: Date)
+data class DataMeta(val dataId: UUID, val created: Long, val updated: Long)
 
 data class ModelMeta(val scriptId: UUID, val modelId: UUID, val dataId: UUID,
-                     val state: RunState, val startTime: Date, val trainedTime: Date? = null, val log: String? = null) {
-    constructor(scriptId: String, modelId: String, dataId: String, state: RunState, startTime: Date) :
+                     val state: RunState, val startTime: Long, val trainedTime: Long? = null, val log: String? = null) {
+    constructor(scriptId: String, modelId: String, dataId: String, state: RunState, startTime: Long) :
             this(scriptId.toUUID(), modelId.toUUID(), dataId.toUUID(), state, startTime)
 }
 
